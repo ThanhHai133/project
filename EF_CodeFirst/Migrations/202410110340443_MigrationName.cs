24403d54853d4bdc78af9833aeaede60a3a7f4a0
hai <haitt200456@gmail.com>
2024-10-11 10:45:57 +0700

    bai 35

---
+++ EF_CodeFirst/Migrations/202410110340443_MigrationName.cs
@@ -0,0 +1,20 @@
+﻿namespace EF_CodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigrationName : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Products", "Price", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AlterColumn("dbo.Products", "DateOfPurchase", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "DateOfPurchase", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Products", "Price", c => c.Decimal(precision: 18, scale: 2));
+        }
+    }
+}
--- EF_CodeFirst/Models/Product.cs
+++ EF_CodeFirst/Models/Product.cs
@@ -10,18 +10,27 @@ namespace EF_CodeFirst.Models
     {
         [Key]
         public long ProductID { get; set; }
-        [Display(Name ="Product Name")]
-        [Required]
+        [Display(Name = "Product Name")]
+        [Required(ErrorMessage = "Product Name cannot be blank")]
+        [RegularExpression(@"^[A-Za-z 0-9]*$", ErrorMessage = "Cannot use special characters in Product Name")]
         public string ProductName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [Required(ErrorMessage = "Price Name cannot be blank")]
+        [Range(0, 100000, ErrorMessage = "Price should be in between 0 to 100000.")]
         public Nullable<decimal> Price { get; set; }
-        [Required]
+      
         public Nullable<System.DateTime> DateOfPurchase { get; set; }
         public string AvailabilityStatus { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "CategoryID Name cannot be blank")]
         public Nullable<long> CategoryID { get; set; }
+
+        [Required(ErrorMessage = "BrandID Name cannot be blank")]
         public Nullable<long> BrandID { get; set; }
+        
         public Nullable<bool> Active { get; set; }
-        [Required]
+  
 
         public virtual Brand Brand { get; set; }
         public virtual Category Category { get; set; }
